c1370d42878fa8d9508197b1eb00611abb6d085a
Robertas Skiauteris <Shalomka@users.noreply.github.com>
2024-07-25 10:40:20 +0300

    added dowload models

--- lib/src/models.dart
+++ lib/src/models.dart
@@ -2,3 +2,4 @@ export 'models/document_model.dart';
 export 'models/elpako_response_base.dart';
 export 'models/enums.dart';
 export 'models/upload_models.dart';
+export 'models/download_models.dart';
